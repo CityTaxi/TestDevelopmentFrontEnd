16e1fcacd4938c0ccb40baadad79d4b1b9d85c7c
Andrés Zapata <andresmauricio030400@gmail.com>
2023-05-25 01:04:02 -0500

    Agregar UserProfile
    Se realizan los siguientes puntos:
    
    1. Se implementa lógica para obtener los datos del usuario seleccionado y el renderizado de dichos datos en el componente UserProfile.
    
    2. Se añaden estilos al archivo UserProfile.module.css para mejorar la apariencia del componente.
    
    3. Se configura ruta protegida para UserProfile en el componente App.

--- test-development-front-end/src/App.js
+++ test-development-front-end/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 import UserSearch from "./components/UserSearch";
+import UserProfile from "./components/UserProfile";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
@@ -15,6 +16,16 @@ function App() {
               isAllowed={localStorage.getItem("accessToken")}
               redirectTo="/"
               children={<UserSearch />}
+            />
+          }
+        />
+        <Route
+          path="/users/:username"
+          element={
+            <ProtectedRoute
+              isAllowed={localStorage.getItem("accessToken")}
+              redirectTo="/"
+              children={<UserProfile />}
             />
           }
         />
