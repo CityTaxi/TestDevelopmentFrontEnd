cb8c4cd7d92ae10fc1d0f79da0b08812de2d1561
Andrés Zapata <andresmauricio030400@gmail.com>
2023-05-25 00:36:35 -0500

    Agregar UserSearch
    Se realizan los siguientes puntos:
    
    1. Se implementa funcionalidad para el cierre de sesión y se agrega icono de FontAwesome en el componente LogOut.
    
    2. Se añaden estilos al archivo LogOut.module.css para personalizar el icono de cierre de sesión.
    
    3. Se implementa lógica para la búsqueda de usuarios mediante la API de GitHub y el renderizado del resultado obtenido en el componente UserSearch.
    
    4. Se añaden estilos al archivo UserSearch.module.css para mejorar la apariencia del componente.
    
    5. Se crea el componente ProtectedRoute y se implementa lógica para proteger las rutas necesarias dentro de la aplicación.
    
    6. Se configura ruta protegida para UserSearch en el componente App.

--- test-development-front-end/src/App.js
+++ test-development-front-end/src/App.js
@@ -1,11 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
+import UserSearch from "./components/UserSearch";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginForm />} />
+        <Route
+          path="/users"
+          element={
+            <ProtectedRoute
+              isAllowed={localStorage.getItem("accessToken")}
+              redirectTo="/"
+              children={<UserSearch />}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
